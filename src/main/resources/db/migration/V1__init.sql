fec5a5d11f1638dc8e913393201222078d4f4b32
jacek.malatynski <jacek.malatynski@jamf.com>
2019-06-30 12:52:13 +0200

    add flyway table aluno

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE aluno (
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  name varchar(20) NOT NULL,
+  surname varchar(40) NOT NULL,
+  apelido varchar(40) DEFAULT "brak",
+  email varchar(50)DEFAULT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY id_surname_apelido (apelido, surname, id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
